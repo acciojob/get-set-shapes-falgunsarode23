a4f023411a51905554c0750a79a8227cff74e7be
Falgun Sarode <147540719+falgunsarode23@users.noreply.github.com>
2024-08-29 20:23:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  // Getter for width
+  get width() {
+    return this._width;
+  }
+
+  // Getter for height
+  get height() {
+    return this._height;
+  }
+
+  // Method to calculate and return the area of the rectangle
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side);
+  }
+
+  // Method to calculate and return the perimeter of the square
+  getPerimeter() {
+    return 4 * this._width;
+  }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
